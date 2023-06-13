a2bf562729e47af4f7415d4874c74c8554c61863
MsMoonglade <moonglade95@gmail.com>
2023-06-13 12:07:31 +0200

    start end game new behaviour

--- Assets/Scripts/Gameplay/EndGameBehaviour.cs
+++ Assets/Scripts/Gameplay/EndGameBehaviour.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using TMPro;
 using Unity.Mathematics;
 using UnityEditor;
@@ -14,15 +15,11 @@ public class EndGameBehaviour : MonoBehaviour
 
     public CinemachineVirtualCamera virtualCamera;
     public GameObject endGameDiamondPrefs;
-    public GameObject endGameDiamondParent;
-    public GameObject endGameDiamondSpawner;
-    public GameObject endGameUiBackground;
-    public GameObject endGameObstaclesParent;
+    public GameObject endGameDiamondBasket;
+    public GameObject endGameCameraTarget;
 
     public GameObject endGameCoinInstantiatePosRef;
 
-    public GameObject endGamebonusGoldUi;
-    public TMP_Text endGameBonusGoldText;
 
     private Coroutine endGamecoroutine;
 
@@ -36,8 +33,7 @@ public class EndGameBehaviour : MonoBehaviour
 
         endGamecoroutine = null;
 
-        endGamebonusGoldUi.transform.localScale = Vector3.zero;
-        endGameUiBackground.transform.localScale = Vector3.zero;
+
     }
 
     public void StartEndGame()
@@ -52,7 +48,6 @@ public class EndGameBehaviour : MonoBehaviour
     public void IncreaseEndGameBonusGold(int amount)
     {
         endGameBonusGold += amount;
-        endGameBonusGoldText.text = endGameBonusGold.ToString();
     }
 
     public void CallOnEndGameButton()
@@ -73,7 +68,40 @@ public class EndGameBehaviour : MonoBehaviour
     {
        GameManager.instance.LoadLevel();
     }
+    private IEnumerator EndGameBehaviourCoroutine()
+    {
+        // endGamebonusGoldUi.transform.DOScale(Vector3.one, 0.5f);
 
+        // yield return new WaitForEndOfFrame();
+
+        virtualCamera.Priority = 500;
+
+        yield return new WaitForSeconds(0.5f);
+
+        int endGameDiamond = ShopManager.instance.currentDiamond;
+        float yPosOffset = 1f;
+
+        for (int i = 0; i < endGameDiamond; i++)
+        {
+            ShopManager.instance.DecreaseDiamond(1);
+            ShopManager.instance.TweenDiamondUi();
+
+            Vector3 spawnPos = Camera.main.ScreenToWorldPoint(UiManager.instance.ui_Diamond_Destination.transform.position);
+            Vector3 endPos = new Vector3(0, i * yPosOffset, 0);
+
+            GameObject diamond = Instantiate(endGameDiamondPrefs, endPos, quaternion.identity, endGameDiamondBasket.transform);
+            diamond.transform.localPosition = endPos;
+            endGameCameraTarget.transform.DOLocalMove(endPos, 0.15f);
+
+            yield return new WaitForSeconds(0.15f);
+        }
+
+        yield return new WaitForSeconds(2.5f);
+    }
+    
+
+    //endgameGambling
+    /*
     private IEnumerator EndGameBehaviourCoroutine()
     {
         endGamebonusGoldUi.transform.DOScale(Vector3.one, 0.5f);
@@ -130,4 +158,5 @@ public class EndGameBehaviour : MonoBehaviour
             .SetEase(Ease.Linear)
             .SetLoops(-1, LoopType.Yoyo);       
     }
+    */
 }
