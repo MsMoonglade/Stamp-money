aa8e0ce4d91b5de75fb6d6cd892994b98f3b9133
MsMoonglade <moonglade95@gmail.com>
2023-06-14 18:26:44 +0200

    end game ender

---
+++ Assets/Scripts/Gameplay/EndGameEnder.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameEnder : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            EventManager.TriggerEvent(Events.endGame);
+
+            Invoke("DelayNewScene", 0.5f);
+        }
+    }
+
+    private void DelayNewScene()
+    {
+        GameManager.instance.LoadLevel();
+    }
+}
